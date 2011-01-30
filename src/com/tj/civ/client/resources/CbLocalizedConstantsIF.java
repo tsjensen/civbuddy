9aac3953a2d16023abb3876219dfb7fbcfd6737f
Thomas Jensen <tsjensen@users.noreply.github.com>
2011-01-30 22:32:55 +0000

    more localization, removed method javadocs

--- src/com/tj/civ/client/resources/CbLocalizedConstantsIF.java
+++ src/com/tj/civ/client/resources/CbLocalizedConstantsIF.java
@@ -27,118 +27,93 @@ import com.google.gwt.i18n.client.Constants;
 public interface CcLocalizedConstantsIF
     extends Constants
 {
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Civilization Cards")
     String civCards();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Funds")
     String funds();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Treasury")
     String treasury();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Card Details")
     String cardDetails();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("< Player")
     String changeUser();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Revise")
     String revise();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Done")
     String reviseDone();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Buy Cards")
     String commit();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("OK")
     String ok();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Cancel")
     String cancel();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("This will clear your plans.")
     String askClearPlans();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Switch to another player")
     String btnTitleChangeUser();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Correct the cards already owned")
     String btnTitleRevise();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Buy the cards flagged as 'planned'")
     String btnTitleBuyCards();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Buy all planned cards?")
     String askCommitBuy();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Insufficient funds")
     String noFunds();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("This may prevent you from winning.")
     String askDiscouraged();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("You may not be able to afford this.")
     String askUnaffordable();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Are you sure?")
     String askAreYouSure();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Points")
     String statsPoints();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Cards")
     String statsCards();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Funds")
     String statsFunds();
 
-    /** accessor method for a localized string constant.
-     *  @return the constant value in the current locale */
     @DefaultStringValue("Groups")
     String statsGroups();
+
+    @DefaultStringValue("Clear")
+    String clearFunds();
+
+    @DefaultStringValue("Set all funds to zero")
+    String clearFundsDesc();
+
+    @DefaultStringValue("This will reset all funds data.")
+    String askClearFunds();
+
+    @DefaultStringValue("On")
+    String on();
+
+    @DefaultStringValue("Off")
+    String off();
+
+    @DefaultStringValue("Enable funds tracking")
+    String enableFunds();
+
+    @DefaultStringValue("Disable funds tracking")
+    String disableFunds();
 }
