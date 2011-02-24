5e618cf03b38f8c597c1f602f3f3c96de728817c
Thomas Jensen <tsjensen@users.noreply.github.com>
2011-02-24 20:36:07 +0000

    CcImages -> CcImagesIF

--- src/com/tj/civ/client/resources/CbImagesIF.java
+++ src/com/tj/civ/client/resources/CbImagesIF.java
@@ -25,7 +25,7 @@ import com.google.gwt.resources.client.ImageResource;
  *
  * @author Thomas Jensen
  */
-public interface CcImages
+public interface CcImagesIF
     extends ClientBundle
 {
     /*
