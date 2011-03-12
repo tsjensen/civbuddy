de0fb76ea5da16c44e7ac55e8697a192637f646a
Thomas Jensen <tsjensen@users.noreply.github.com>
2011-03-12 14:49:07 +0000

    Initial Revision

---
+++ src/com/tj/civ/client/model/jso/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The JavaScript Overlay (JSO) Types.
+ * 
+ * @author Thomas Jensen
+ */
+package com.tj.civ.client.model.jso;
---
+++ src/com/tj/civ/client/model/vo/CbVariantVO.java
@@ -0,0 +1,80 @@
+/*
+ * CivCounsel - A Civilization Tactics Guide
+ * Copyright (c) 2011 Thomas Jensen
+ * $Id$
+ * Date created: 11.03.2011
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License Version 2 as published by the Free
+ * Software Foundation.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.tj.civ.client.model.vo;
+
+
+/**
+ * View object representing those attributes of a
+ * {@link com.tj.civ.client.model.CcVariantConfig} that are shown in the list of
+ * variants of the 'Variants' view.
+ *
+ * @author Thomas Jensen
+ */
+public class CcVariantVO
+    extends CcAbstractViewObject
+{
+    /** the variant ID */
+    private String iVariantId;
+
+    /** the localized variant name */
+    private String iVariantNameLocalized;
+
+    /** <code>true</code> if the variant of this {@lin #iVariantId} is unknown */
+    private boolean iUnknown;
+
+
+
+    /**
+     * Constructor.
+     * @param pVariantId the variant ID
+     * @param pVariantNameLocalized the localized variant name (may be
+     *          <code>null</code> if the vairant is unknown)
+     */
+    public CcVariantVO(final String pVariantId, final String pVariantNameLocalized)
+    {
+        super();
+        iVariantId = pVariantId;
+        if (pVariantNameLocalized != null) {
+            iVariantNameLocalized = pVariantNameLocalized;
+            iUnknown = false;
+        } else {
+            iVariantNameLocalized = pVariantId + " (unknown)";
+            iUnknown = true;
+        }
+    }
+
+
+
+    public String getVariantId()
+    {
+        return iVariantId;
+    }
+
+
+
+    public String getVariantNameLocalized()
+    {
+        return iVariantNameLocalized;
+    }
+
+
+
+    public boolean isUnknown()
+    {
+        return iUnknown;
+    }
+}
