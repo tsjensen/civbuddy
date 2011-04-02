acdfd301669466d7834b580ed1d0f260ff0718db
tsjensen <>
2011-04-02 20:39:04 +0000

    minor checkstyle

--- src/com/tj/civ/icongen/CbIconGen.java
+++ src/com/tj/civ/icongen/CbIconGen.java
@@ -1,13 +1,18 @@
 /*
- * CivCounsel - A Civilization Tactics Guide Copyright (c) 2010 Thomas Jensen $Id$
- * Date created: 25.12.2010 This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General Public License Version 2 as
- * published by the Free Software Foundation. This program is distributed in the hope
- * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
- * License for more details. You should have received a copy of the GNU General Public
- * License along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * CivBuddy - A Civilization Tactics Guide
+ * Copyright (c) 2010 Thomas Jensen
+ * $Id$
+ * Date created: 25.12.2010
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License Version 2 as published by the Free
+ * Software Foundation.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
  */
 package com.tj.civ.icongen;
 
@@ -234,6 +239,7 @@ public final class CcIconGen
     {
         final int s = pGroupIconSizePx / 2 - 1;
         final int h = s / 2;
+        @SuppressWarnings("cast")
         final int v = (int) Math.round(Math.sin(Math.toRadians(60)) * ((float) s));
 
         Polygon result = new Polygon();
