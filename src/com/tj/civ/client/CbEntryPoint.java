ce451320f620d6ef84fe07ca749d133696af3985
Thomas Jensen <tsjensen@users.noreply.github.com>
2011-04-02 20:46:04 +0000

    major rework as controllers move to MVP, much simpler now

--- src/com/tj/civ/client/CbEntryPoint.java
+++ src/com/tj/civ/client/CbEntryPoint.java
@@ -1,204 +1,63 @@
+/*
+ * CivBuddy - A Civilization Tactics Guide
+ * Copyright (c) 2011 Thomas Jensen
+ * $Id$
+ * Date created: 2011-01-11
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License Version 2 as published by the Free
+ * Software Foundation.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package com.tj.civ.client;
 
 import com.google.gwt.activity.shared.ActivityManager;
 import com.google.gwt.activity.shared.ActivityMapper;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.logical.shared.BeforeSelectionEvent;
-import com.google.gwt.event.logical.shared.BeforeSelectionHandler;
+import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.place.shared.PlaceController;
 import com.google.gwt.place.shared.PlaceHistoryHandler;
-import com.google.gwt.safehtml.shared.SafeHtmlUtils;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
-import com.google.gwt.user.client.ui.TabBar;
-import com.google.gwt.user.client.ui.TabPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
-import com.tj.civ.client.common.CcUtil;
-import com.tj.civ.client.event.CcAllStatesEvent;
-import com.tj.civ.client.event.CcAllStatesHandlerIF;
-import com.tj.civ.client.event.CcEventBus;
-import com.tj.civ.client.event.CcFundsEvent;
-import com.tj.civ.client.event.CcStateEvent;
-import com.tj.civ.client.event.CcStateHandlerIF;
-import com.tj.civ.client.model.CcCardCurrent;
-import com.tj.civ.client.model.CcGame;
-import com.tj.civ.client.model.CcSituation;
-import com.tj.civ.client.model.CcSituationMock;
-import com.tj.civ.client.model.CcState;
-import com.tj.civ.client.model.CcVariantConfig;
-import com.tj.civ.client.model.CcVariantConfigMock;
-import com.tj.civ.client.model.jso.CcGameJSO;
 import com.tj.civ.client.resources.CcClientBundleIF;
 import com.tj.civ.client.resources.CcConstants;
-import com.tj.civ.client.widgets.CcMessageBox;
-import com.tj.civ.client.widgets.CcMessageBox.CcResultCallbackIF;
-import com.tj.civ.client.widgets.CcStatistics;
 
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class CcEntryPoint
-    implements EntryPoint, CcStateHandlerIF, CcAllStatesHandlerIF
+    implements EntryPoint
 {
     /** logger for this class */
     //private static final Logger LOG = Logger.getLogger(CcEntryPoint.class.getName());
-
-    /** Tab index of the 'funds' panel */
-    private static final int TABPANEL_FUNDS_IDX = 1;
-
-    /** the main tab panel allowing change between the funds and cards tabs */
-    private TabPanel iTabPanel = null;
-
-    /** the buy cards button */
-    private Button iBtnCommit = null;
-    
-    /** the revise button */
-    private Button iBtnRevise = null;
-    
-    /** the card controller */
-    private CcCardController iCardCtrl = null;
 
     /** the topmost widget */
     private SimplePanel iAppWidget = new SimplePanel();
 
 
 
-    private CcGame mockGame(final CcVariantConfig pVariant)
-    {
-        CcGameJSO gameJso = CcGameJSO.create();
-        gameJso.setName("2011-01-05 Mock Game"); //$NON-NLS-1$
-        gameJso.setVariantId(pVariant.getVariantId());
-        CcGame result = new CcGame(gameJso);
-        CcSituation sit = new CcSituationMock(pVariant);
-        sit.recalc();
-        result.addPlayer(sit);
-        return result;
-    }
-
-
-
-    private void toggleReviseMode()
-    {
-        if (iCardCtrl.isRevising())
-        {
-            iCardCtrl.leaveReviseMode();
-            iBtnRevise.setText(CcConstants.STRINGS.revise());
-            iTabPanel.getTabBar().setTabEnabled(TABPANEL_FUNDS_IDX, true);
-            // leave commit button disabled
-        }
-        else {
-            iBtnCommit.setEnabled(false);
-            iBtnRevise.setText(CcConstants.STRINGS.reviseDone());
-            iTabPanel.getTabBar().setTabEnabled(TABPANEL_FUNDS_IDX, false);
-            iCardCtrl.enterReviseMode();
-        }
-    }
-
-
-
-    private Panel createCardButtonPanel()
-    {
-        iBtnCommit = new Button(CcConstants.STRINGS.commit());
-        iBtnCommit.setStyleName(CcConstants.CSS.ccButton());
-        iBtnCommit.setTitle(CcConstants.STRINGS.btnTitleBuyCards());
-        iBtnCommit.setEnabled(false);
-        iBtnCommit.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(final ClickEvent pEvent)
-            {
-                if (iCardCtrl.hasAnyPlans()) {
-                    iCardCtrl.commit();
-                }
-            }
-        });
-
-        final Button btnChangeUser = new Button(CcConstants.STRINGS.changeUser());
-        btnChangeUser.setStyleName(CcConstants.CSS.ccButton());
-        btnChangeUser.setTitle(CcConstants.STRINGS.btnTitleChangeUser());
-        btnChangeUser.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(final ClickEvent pEvent)
-            {
-                // TODO Change User Handler
-                Window.alert(CcUtil.getUuid());
-            }
-        });
-
-        iBtnRevise = new Button(CcConstants.STRINGS.revise());
-        iBtnRevise.setStyleName(CcConstants.CSS.ccButton());
-        iBtnRevise.setTitle(CcConstants.STRINGS.btnTitleRevise());
-        iBtnRevise.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(final ClickEvent pEvent)
-            {
-                if (iCardCtrl.isRevising() || !iCardCtrl.hasAnyPlans()) {
-                    toggleReviseMode();
-                } else {
-                    CcMessageBox.showOkCancel(CcConstants.STRINGS.askAreYouSure(),
-                        SafeHtmlUtils.fromString(CcConstants.STRINGS.askClearPlans()),
-                        iTabPanel, new CcResultCallbackIF() {
-                            @Override
-                            public void onResultAvailable(final boolean pResult)
-                            {
-                                if (pResult) {
-                                    toggleReviseMode();
-                                }
-                            }
-                        }
-                    );
-                }
-            }
-        });
-
-        HorizontalPanel result = new HorizontalPanel();
-        result.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LEFT);
-        result.add(btnChangeUser);
-        result.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
-        result.add(iBtnRevise);
-        result.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
-        result.add(iBtnCommit);
-        result.setStyleName(CcConstants.CSS.ccButtonPanel() + " " //$NON-NLS-1$
-            + CcConstants.CSS_BLUEGRADIENT);
-        return result;
-    }
-
-
-
     /**
      * This is the entry point method.
      */
+    @Override
     public void onModuleLoad()
     {
-        // BEGIN MOCK
-        final CcVariantConfig variant = new CcVariantConfigMock();
-        final CcGame game = mockGame(variant);
-        final CcSituation sit = game.getSituations().values().iterator().next();
-        final CcCardCurrent[] cardsCurrent = sit.getCardsCurrent();
-        // END MOCK
-        
         // Inject CSS
         CcClientBundleIF.INSTANCE.css().ensureInjected();
 
         CcClientFactoryIF clientFactory = GWT.create(CcClientFactoryIF.class);
         PlaceController placeController = clientFactory.getPlaceController();
-        final CcEventBus eventBus = clientFactory.getEventBus();
-        eventBus.setSituation(sit);
+        final EventBus eventBus = clientFactory.getEventBus();
         iAppWidget.setWidth("320px");
         
-        // TODO remove
-        System.out.println(variant.toJson());
-        //System.out.println(variant.getCommodities()[0].getNames().getStringI18n("fi"));
-
         // Start ActivityManager for the main widget with our ActivityMapper
         ActivityMapper activityMapper = new CcActivityMapper(clientFactory);
         ActivityManager activityManager = new ActivityManager(activityMapper, eventBus);
@@ -209,91 +68,10 @@ public class CcEntryPoint
         PlaceHistoryHandler historyHandler = new PlaceHistoryHandler(historyMapper);
         historyHandler.register(placeController, eventBus, CcConstants.DEFAULT_PLACE);
 
-        final CcFundsController fundsCtrl = new CcFundsController(sit);
-        iCardCtrl = new CcCardController(cardsCurrent, new CcCardStateManager(variant,
-            sit.getPlayer().getWinningTotal()));
-        iCardCtrl.init();
-
-        CcStatistics stats = new CcStatistics(sit.getPlayer().getWinningTotal(),
-            sit.getVariant().getNumCardsLimit());
-        VerticalPanel outerVP = new VerticalPanel();
-        outerVP.add(createCardButtonPanel());
-        outerVP.add(stats);
-        outerVP.add(iCardCtrl.getGrid());
-
-        final TabPanel tp = new TabPanel();
-        tp.add(outerVP, CcConstants.STRINGS.civCards());
-        tp.add(fundsCtrl.getPanel(), CcConstants.STRINGS.funds());
-        tp.selectTab(CcConstants.TABNUM_CARDS);
-        tp.setStyleName(CcConstants.CSS.ccTabPanel(), true);
-        tp.getDeckPanel().setStyleName(CcConstants.CSS.ccTabPanelBottom());
-        //tp.getDeckPanel().setAnimationEnabled(true);
-        tp.getTabBar().addBeforeSelectionHandler(new BeforeSelectionHandler<Integer>() {
-            @Override
-            public void onBeforeSelection(final BeforeSelectionEvent<Integer> pEvent)
-            {
-                int current = ((TabBar) pEvent.getSource()).getSelectedTab();
-                int sel = pEvent.getItem().intValue();
-                if (sel == CcConstants.TABNUM_CARDS && current != CcConstants.TABNUM_CARDS) {
-                    eventBus.fireEventFromSource(
-                        new CcFundsEvent(fundsCtrl.getFunds(), fundsCtrl.isEnabled()), tp);
-                }
-            }
-        });
-        iTabPanel = tp;
-
         // Add it to the root panel.
-        //RootPanel.get(CcConstants.INJECTION_POINT).add(tp);  // TODO change back
         RootPanel.get(CcConstants.INJECTION_POINT).add(iAppWidget);
-
-        // register event handlers
-        eventBus.addHandlerToSource(CcAllStatesEvent.TYPE, iCardCtrl, this);
-        eventBus.addHandlerToSource(CcStateEvent.TYPE, iCardCtrl, this);
-
-        // trigger initial calculation of all state information
-        eventBus.fireEventFromSource(new CcAllStatesEvent(), this);
 
         // Goes to the place represented on URL else default place
         historyHandler.handleCurrentHistory();
-    }
-
-
-
-    @Override
-    public void onAllStatesChanged(final CcAllStatesEvent pEvent)
-    {
-        updateCommitButton(null);
-    }
-
-
-
-    @Override
-    public void onStateChanged(final CcStateEvent pEvent)
-    {
-        updateCommitButton(pEvent.getSituation().getCardsCurrent()[pEvent.getRowIdx()]);
-    }
-
-
-
-    /**
-     * Enable/disable commit button.
-     * @param pCard if an individual card was changed, that card; else if it's a
-     *              global update, just <code>null</code>
-     */
-    private void updateCommitButton(final CcCardCurrent pCard)
-    {
-        if (iBtnCommit.isEnabled()) {
-            if (!iCardCtrl.hasAnyPlans()) {
-                iBtnCommit.setEnabled(false);
-            }
-        } else {
-            if (pCard != null) {
-                if (pCard.getState() == CcState.Planned) {
-                    iBtnCommit.setEnabled(true);
-                }
-            } else if (iCardCtrl.hasAnyPlans()) {
-                iBtnCommit.setEnabled(true);
-            }
-        }
     }
 }
