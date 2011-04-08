069b9b77794caca19fa2346ad89b996b6c139ddc
Thomas Jensen <tsjensen@users.noreply.github.com>
2011-04-08 08:57:26 +0000

    started isDiscouraged() reimplementation with much faster algorithm

--- src/com/tj/civ/client/CbCardStateManager.java
+++ src/com/tj/civ/client/CbCardStateManager.java
@@ -17,6 +17,7 @@
 package com.tj.civ.client;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
@@ -76,14 +77,21 @@ public class CcCardStateManager
      * @param pTargetPoints target points of this player's civilization. This value
      *          should be 0 (zero) if the game variant does not feature a card
      *          limit
+     * @param pFundsEnabled initial value of the funds enabled flag, will be
+     *          updated through Funds events
+     * @param pFundsTotal initial value of the total funds, will be updated through
+     *          Funds events
      */
     public CcCardStateManager(final CbCardsViewIF.CcPresenterIF pActivity,
-        final CcVariantConfig pVariant, final int pTargetPoints)
+        final CcVariantConfig pVariant, final int pTargetPoints,
+        final boolean pFundsEnabled, final int pFundsTotal)
     {
         super();
         iPresenter = pActivity;
         iVariant = pVariant;
         iTargetPoints = pTargetPoints;
+        iFundsEnabled = pFundsEnabled;
+        iFundsTotal = pFundsTotal;
 
         pActivity.getEventBus().addHandler(CcFundsEvent.TYPE, new CcFundsHandlerIF() {
             @Override
@@ -106,16 +114,30 @@ public class CcCardStateManager
         if (LOG.isLoggable(Level.FINE)) {
             debugTimeStart = System.currentTimeMillis();
         }
-        StatesForSpecial = null;
+        if (LOG.isLoggable(Level.FINER)) {
+            LOG.finer("ENTER: recalcAll()"); //$NON-NLS-1$
+        }
         final CcCardCurrent[] cardsCurrent = iPresenter.getCardsCurrent();
         for (CcCardCurrent card : cardsCurrent)
         {
             final CcCardConfig cardConfig = card.getConfig();
             final CcState currentState = card.getState();
             CcState newState = null;
-            String reason = ""; //$NON-NLS-1$
+            String reason = null;
+            if (LOG.isLoggable(Level.FINE)) {
+                LOG.fine("recalcAll(): Computing " + card.getConfig() //$NON-NLS-1$
+                    + " (state=" + currentState + ")"); //$NON-NLS-1$ //$NON-NLS-2$
+            }
+            if (card.getMyIdx() == 1) {
+                LOG.setLevel(Level.FINEST);  // TODO remove
+            } else {
+                LOG.setLevel(Level.FINER);
+            }
 
             if (currentState.isAffectingCredit()) {
+                if (LOG.isLoggable(Level.FINE)) {
+                    LOG.fine("recalcAll(): - - - - - - - - - - - - - - - - - - - - - - - -> Skipping; Owned or Planned"); //$NON-NLS-1$
+                }
                 continue;   // Owned and Planned
             }
 
@@ -125,28 +147,48 @@ public class CcCardStateManager
                 newState = CcState.PrereqFailed;
                 String prn = cardsCurrent[cardConfig.getPrereq()].getConfig().getLocalizedName();
                 reason = CcConstants.MESSAGES.prereqFailed(prn);
+                if (LOG.isLoggable(Level.FINE)) {
+                    LOG.fine("recalcAll(): - - - - - - - - - - - - - - - - - - - - - - - -> Prereq Failed (" + prn //$NON-NLS-1$
+                        + ")"); //$NON-NLS-1$
+                }
             }
             else if (iFundsEnabled
                 && (iFundsTotal - iPresenter.getPlannedInvestment() - card.getCostCurrent()) < 0)
             {
                 newState = CcState.Unaffordable;
                 reason = CcConstants.STRINGS.noFunds();
+                if (LOG.isLoggable(Level.FINE)) {
+                    LOG.fine("recalcAll(): - - - - - - - - - - - - - - - - - - - - - - - -> unaffordable"); //$NON-NLS-1$
+                }
             }
             else {
-                final int pointsAchievable = computePointsAchievable(card.getMyIdx());
+                final int pointsAchievable = computePointsAchievableOld(card.getMyIdx());
                 if (isDiscouraged(pointsAchievable)) {
                     newState = CcState.DiscouragedBuy;
                     reason = CcConstants.MESSAGES.discouraged(iTargetPoints - pointsAchievable);
+                    if (LOG.isLoggable(Level.FINE)) {
+                        LOG.fine("recalcAll(): - - - - - - - - - - - - - - - - - - - - - - - -> discouraged"); //$NON-NLS-1$
+                    }
                 }
                 else {
                     newState = CcState.Absent;
+                    if (LOG.isLoggable(Level.FINE)) {
+                        LOG.fine("recalcAll(): - - - - - - - - - - - - - - - - - - - - - - - -> Absent"); //$NON-NLS-1$
+                    }
                 }
             }
-            // TODO: erst alle states berechnen, dann anzeigen
+            // TODO: erst alle states berechnen, dann anzeigen (generell behandeln)
             // TODO: falls alle verbleibenden karten rot sind, anders anzeigen
+            if (LOG.isLoggable(Level.FINE)) {
+                LOG.fine("recalcAll(): currentState=" + currentState //$NON-NLS-1$
+                    + "; newState=" + newState); //$NON-NLS-1$
+            }
             if (currentState != newState) {
                 iPresenter.setState(card, newState, reason);
             }
+        }
+        if (LOG.isLoggable(Level.FINER)) {
+            LOG.finer("EXIT: recalcAll()"); //$NON-NLS-1$
         }
         if (LOG.isLoggable(Level.FINE)) {
             LOG.fine("recalcAll() - TIME: " //$NON-NLS-1$
@@ -219,10 +261,12 @@ public class CcCardStateManager
 
 
     
-    private int computePointsAchievable(final int pRowIdx)
+    private int computePointsAchievableOld(final int pRowIdx)
     {
-        if (LOG.isLoggable(Level.FINER)) {
-            LOG.finer("========= Computing '" //$NON-NLS-1$
+        if (LOG.isLoggable(Level.FINEST)) {
+            LOG.finest("ENTER: computePointsAchievable(" + pRowIdx //$NON-NLS-1$
+                + ")"); //$NON-NLS-1$
+            LOG.finest("========= Computing '" //$NON-NLS-1$
                 + iPresenter.getCardsCurrent()[pRowIdx].getConfig().getLocalizedName()
                 + "' ===================================="); //$NON-NLS-1$
         }
@@ -231,13 +275,14 @@ public class CcCardStateManager
             final int remainingSteps = Math.max(0,
                 iVariant.getNumCardsLimit() - iPresenter.getNumCardsAffectingCredit());
             final List<Integer> path = new ArrayList<Integer>();
-            path.add(realIdx2SpecialIdx(pRowIdx));
             CcCardConfig[] specialCards = iVariant.getCardsSpeciallySorted();
+            if (LOG.isLoggable(Level.FINER)) {
+                LOG.finer("speciallySorted = " + Arrays.deepToString(specialCards)); //$NON-NLS-1$
+            }
             Integer startingPoint = null;
             for (int i = 0; i < specialCards.length; i++) {
                 if (!getStateFromSpecial(iPresenter.getCardsCurrent(),
                         Integer.valueOf(i)).isAffectingCredit()
-                    && specialCards[i].getMyIdx() != pRowIdx
                     && isPrereqMet(iPresenter.getCardsCurrent(), specialCards[i], path))
                 {
                     startingPoint = Integer.valueOf(i);
@@ -248,15 +293,21 @@ public class CcCardStateManager
             //        Democracy und Philosophy gekauft werden dürfen, wird irrtümlich
             //        Democracy als discouraged angezeigt, wenn man Philosophy zuerst
             //        wählt. Andersrum ist alles ok.
+            // FIXME: 102 Funds, Pottery, Cloth, Eng Owned, D&P Planned -> Myst not discouraged!
             if (startingPoint != null) {
+                if (LOG.isLoggable(Level.FINEST)) {
+                    LOG.finest("Entering into isDiscouragedInternal() with:" //$NON-NLS-1$
+                        +  "\n   - pSpecialIdx     = " + startingPoint     //$NON-NLS-1$
+                        +  "\n   - pPath           = " + path              //$NON-NLS-1$
+                        +  "\n   - pSum      = " + iPresenter.getNominalSumInclPlan() //$NON-NLS-1$
+                        +  "\n   - pRemainingSteps = " + remainingSteps); //$NON-NLS-1$
+                }
                 result = isDiscouragedInternal(startingPoint, path,
-                    iPresenter.getNominalSumInclPlan()
-                    + iVariant.getCards()[pRowIdx].getCostNominal(),
-                    remainingSteps - 1);
+                    iPresenter.getNominalSumInclPlan(), remainingSteps);
             }
         }
-        if (LOG.isLoggable(Level.FINER)) {
-            LOG.finer("Done. Result=" + result);  //$NON-NLS-1$
+        if (LOG.isLoggable(Level.FINEST)) {
+            LOG.finest("Done. Result=" + result);  //$NON-NLS-1$
         }
         return result;
     }
@@ -276,8 +327,8 @@ public class CcCardStateManager
     private int isDiscouragedInternal(final Integer pSpecialIdx,
         final List<Integer> pPath, final int pSum, final int pRemainingSteps)
     {
-        if (LOG.isLoggable(Level.FINER)) {
-            LOG.finer("isDiscouragedInternal() - ENTER - pSpecialIdx=" //$NON-NLS-1$
+        if (LOG.isLoggable(Level.FINEST)) {
+            LOG.finest("isDiscouragedInternal() - ENTER - pSpecialIdx=" //$NON-NLS-1$
                 + pSpecialIdx + ", pPath=" + pPath                     //$NON-NLS-1$
                 + ", pSum=" + pSum                                     //$NON-NLS-1$
                 + ", pRemainingSteps=" + pRemainingSteps);             //$NON-NLS-1$
@@ -304,8 +355,8 @@ public class CcCardStateManager
                     pPath.remove(pPath.size() - 1);
                     if (newSum >= iTargetPoints) {
                         result = newSum;
-                        if (LOG.isLoggable(Level.FINER)) {
-                            LOG.finer("OK, path = " + pPath); //$NON-NLS-1$
+                        if (LOG.isLoggable(Level.FINEST)) {
+                            LOG.finest("OK, path = " + pPath); //$NON-NLS-1$
                         }
                         break;  // great, we're ok
                     }
@@ -315,9 +366,10 @@ public class CcCardStateManager
                 }
             }
         }
-        if (LOG.isLoggable(Level.FINER)) {
-            LOG.finer("isDiscouragedInternal() - EXIT - result = " + result); //$NON-NLS-1$
+        if (LOG.isLoggable(Level.FINEST)) {
+            LOG.finest("isDiscouragedInternal() - EXIT - result = " + result); //$NON-NLS-1$
         }
+        LOG.setLevel(Level.FINER); // TODO remvece
         return result;
     }
     
@@ -344,6 +396,107 @@ public class CcCardStateManager
                 }
             }
         }
+        return result;
+    }
+
+
+
+    private boolean isDiscouragedNeu(final int pRowIdx)
+    {
+        if (LOG.isLoggable(Level.FINEST)) {
+            LOG.finest("ENTER: isDiscouraged(" + pRowIdx //$NON-NLS-1$
+                + ")"); //$NON-NLS-1$
+            LOG.finest("========= Computing '" //$NON-NLS-1$
+                + iPresenter.getCardsCurrent()[pRowIdx].getConfig().getLocalizedName()
+                + "' ===================================="); //$NON-NLS-1$
+        }
+        boolean result = false;
+        if (iVariant.getNumCardsLimit() > 0 && iPresenter.getNumCardsAffectingCredit() > 0)
+        {
+            int remainingSteps = Math.max(0,
+                iVariant.getNumCardsLimit() - iPresenter.getNumCardsAffectingCredit());
+            final int fixedNominal = iVariant.getCards()[pRowIdx].getCostNominal();
+            if (remainingSteps == 1)
+            {
+                result = iPresenter.getNominalSumInclPlan() + fixedNominal < iTargetPoints;
+            }
+            else if (remainingSteps > 1)
+            {
+                final CcCardCurrent[] cardsCurrent = iPresenter.getCardsCurrent();
+                final CcCardConfig[] cardsSorted = iVariant.getCardsSortedInternal();
+                if (LOG.isLoggable(Level.FINER)) {
+                    LOG.finer("cardsSorted = " + Arrays.deepToString(cardsSorted)); //$NON-NLS-1$
+                }
+    
+                remainingSteps--;
+                final int[] path = new int[remainingSteps];
+                final int[] rest = new int[cardsSorted.length - (remainingSteps + 1)
+                                           - iPresenter.getNumCardsAffectingCredit()];
+                for (int i = 0, stepsTaken = 0;
+                    i < cardsSorted.length && stepsTaken < remainingSteps; i++)
+                {
+                    int idx = cardsSorted[i].getMyIdx();
+                    CcState state = cardsCurrent[idx].getState();
+                    if (!state.isAffectingCredit() && idx != pRowIdx) {
+                        if (stepsTaken < remainingSteps) {
+                            path[stepsTaken] = i;
+                        } else {
+                            rest[stepsTaken - remainingSteps] = i;
+                        }
+                        stepsTaken++;
+                    }
+                }
+                result = handlePrereqs(
+                    iPresenter.getNominalSumInclPlan() + fixedNominal, path, rest);
+            }
+        }
+        if (LOG.isLoggable(Level.FINEST)) {
+            LOG.finest("EXIT - result = " + result);  //$NON-NLS-1$
+        }
+        return result;
+    }
+
+
+
+    /**
+     * Determine if the required winning total can be reached
+     *  given <tt>pPathSpecial</tt> is legal and if so, calculate
+     * its value. The value returned must be the highest legal value (if goal is
+     * met), or the highest, possible illegal, value (if goal is not met).
+     * <p>In other words
+     * <p>Cards which are already Owned or Planned are never contained in either
+     * array. The values of the cards referenced in both arrays are in descending
+     * order.
+     * @param pStartingSum the sum of all cards already Owned or Planned, plus the
+     *          value of the card whose Discouraged state we are calculating
+     * @param pPathSpecial a potential path, built from the most valuable cards
+     *          still available. Contains indexes into the sorted array of cards.
+     *          Length is always equal to the number of steps the path may have,
+     *          which is at least 1. This is certainly the path with the highest
+     *          value, however it may be illegal due to failed prerequisites.
+     * @param pRestSpecial the remaining card which could theoretically be used on
+     *          the path, in case the path contained illegal entries. Contains
+     *          indexes into the sorted array of cards.
+     * @return <code>true</code> if discouraged, <code>false</code> if okay
+     */
+    private boolean handlePrereqs(final int pStartingSum, final int[] pPathSpecial,
+        final int[] pRestSpecial)
+    {
+        if (LOG.isLoggable(Level.FINEST)) {
+            LOG.finest("ENTER: handlePrereqs(" + pStartingSum //$NON-NLS-1$
+                + ", " + pPathSpecial + ", " + pRestSpecial  //$NON-NLS-1$ //$NON-NLS-2$
+                + ")"); //$NON-NLS-1$
+        }
+        boolean result = false;
+
+        // TODO Auto-generated method stub HERE
+        // TODO Falls ein Eintrag in path die prereqs nicht erfüllt, mit einem
+        //      aus rest tauschen. Über Varianten mti mehreren rprereqs und
+        //      transitiven prereqs nachdenken. Das muss dann aber nicht mehr effizient
+        //      sein. die entscheidung über den initialen path muss sehr schnell
+        //      erfolgen.
+        // Mtheonds: wert-des-pfades, illegal-index
+
         return result;
     }
 }
