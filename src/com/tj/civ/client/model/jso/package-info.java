57c150dbe03799298b837577af87437a8d3e83ef
tsjensen <>
2011-05-01 12:01:41 +0000

    added a small additional bit of info

--- src/com/tj/civ/client/model/jso/package-info.java
+++ src/com/tj/civ/client/model/jso/package-info.java
@@ -1,5 +1,6 @@
 /**
- * The JavaScript Overlay (JSO) Types.
+ * JavaScript Overlay (JSO) Types. These are the part of the model which is being
+ * persisted into HTML5 local storage as JSON strings.
  * 
  * @author Thomas Jensen
  */
