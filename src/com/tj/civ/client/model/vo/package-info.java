9ae9d0d63aab4c7ee360c61482d8283655a5fae3
Thomas Jensen <tsjensen@users.noreply.github.com>
2011-05-01 12:01:14 +0000

    Initial Revision

---
+++ src/com/tj/civ/client/model/vo/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * View objects are dumbed-down copies of their corresponding "regular" model
+ * objects which the views use as input data.
+ * 
+ * @author Thomas Jensen
+ */
+package com.tj.civ.client.model.vo;
