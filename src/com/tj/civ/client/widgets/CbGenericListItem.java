5c5e6e4cc7322001b79b340f4cfbbb273b0b1c44
Thomas Jensen <tsjensen@users.noreply.github.com>
2011-10-22 22:57:57 +0000

    Initial Revision

---
+++ src/com/tj/civ/client/widgets/CbGenericListItem.java
@@ -0,0 +1,42 @@
+/*
+ * CivBuddy - A Civilization Tactics Guide
+ * Copyright (c) 2011 Thomas Jensen
+ * $Id$
+ * Date created: 23.10.2011
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License Version 2 as published by the Free
+ * Software Foundation.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.tj.civ.client.widgets;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+
+/**
+ * TODO Description, ending with a dot.
+ *
+ * @author Thomas Jensen
+ */
+public class CbGeneralListItem
+    extends Composite
+{
+
+    /**
+     * Constructor.
+     */
+    public CbGeneralListItem(final boolean pHasSelector, final boolean pHasMore,
+        final Widget pContentWidget)
+    {
+        // TODO Auto-generated constructor stub
+        // TODO statt boolean wohl eher ein callback
+    }
+
+}
