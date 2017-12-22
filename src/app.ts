ebde4982584a69cc148c570885542000a036f10d
Thomas Jensen <tsjensen@users.noreply.github.com>
2017-12-22 18:27:27 +0100

    First successful build

--- src/app.ts
+++ src/app.ts
@@ -5,24 +5,22 @@ import { RulesJson, CardJson, Language } from './model/rules';
 
 export function hello() {
     let obj2: RulesJson = <any>jsonOriginal;
-    let law: CardJson | undefined = obj2.cards.get("Law");
+    let law: CardJson | undefined = obj2.cards['Law'];
     if (law !== undefined) {
-        console.log("We have cards: " + law.attributes.get(Language.EN));
+        console.log("We have cards: " + law.attributes[Language.EN]);
     } else {
         console.log("barf");
     }
     //const word = Object.keys((<any>jsonOriginal).cards).length;
-    //console.log(word);
     console.log("world");
 }
 
-// activate first input field after showing modal
 /*
+// activate first input field after showing modal
 $('#newGameModal').on('shown.bs.modal', function () {
     setTimeout(function (){
         $('#inputGameName').focus();
     }, 300);  // was: 1000 TODO
-
 })
 */
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,11 @@ module.exports = {
     },
     plugins: [
         new CopyWebpackPlugin([
-            {from: 'resources', to: 'build/dist'},
-            {from: 'vendor/*', to: 'build/dist'}
+            {from: 'resources', to: 'build/dist', ignore: ['rules/*.json']},
+            {from: 'resources/rules', to: 'build/ts/rules'},
+            {from: 'vendor/bootswatch-darkly-v4.0.0-beta.2', to: 'build/dist'},
+            {from: 'vendor/bootstrap-4.0.0-beta.2', to: 'build/dist'},
+            {from: 'vendor/openiconic-1.1.0', to: 'build/dist'}
         ], {
             copyUnmodified: false
         })
