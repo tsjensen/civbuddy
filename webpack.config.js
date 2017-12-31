d7505b83c03bce9b10f02c11de94019032245ac4
Thomas Jensen <tsjensen@users.noreply.github.com>
2017-12-31 10:31:27 +0100

    Get bootstrap from npm instead of vendor folder

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
             {from: 'resources', to: 'build/dist', ignore: ['rules/*.json']},
             {from: 'resources/rules', to: 'build/ts/rules'},
             {from: 'vendor/bootswatch-darkly-v4.0.0-beta.2', to: 'build/dist'},
-            {from: 'vendor/bootstrap-4.0.0-beta.2', to: 'build/dist'},
+            {from: 'node_modules/bootstrap/dist', to: 'build/dist', ignore: ['.DS_Store']},
             {from: 'vendor/openiconic-1.1.0', to: 'build/dist'},
             {from: 'node_modules/l20n/dist/web/l20n.min.js', to: 'build/dist/js'}
         ], {
