50b5330f0ad3fed08f4eb75bdc503f2549d32f77
Thomas Jensen <tsjensen@users.noreply.github.com>
2018-02-21 19:05:54 +0100

    Convert our CSS to SASS

--- webpack.config.js
+++ webpack.config.js
@@ -32,8 +32,7 @@ module.exports = {
         new CopyWebpackPlugin([
             {from: 'resources', to: 'build/dist', ignore: ['rules/*.json', '**/*.html']},
             {from: 'resources/rules', to: 'build/ts/rules'},
-            {from: 'node_modules/bootswatch/dist/darkly', to: 'build/dist/css'},
-            {from: 'node_modules/bootstrap/dist', to: 'build/dist', ignore: ['.DS_Store']},
+            {from: 'node_modules/bootstrap/dist/js', to: 'build/dist/js', ignore: ['.DS_Store']},
             {from: 'node_modules/open-iconic/font/css/open-iconic-bootstrap.min.css', to: 'build/dist/css'},
             {from: 'node_modules/open-iconic/font/fonts', to: 'build/dist/fonts'},
             {from: 'node_modules/l20n/dist/web/l20n.min.js', to: 'build/dist/js'}
